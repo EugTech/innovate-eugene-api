a7277ca6a15ad92f92712eccaf0eb65e25726d5a
John Nelson <me@johnrnelson.com>
2019-03-24 18:20:58 -0700

    it's better but still not working!

--- Tools/BuildObjectMap.js
+++ Tools/BuildObjectMap.js
@@ -10,9 +10,10 @@
 
 const DATA_FOLDER = require("../LIB/DATA_FOLDER");
 
-
+//The master map of all data...
 const ObjectMapData = {};
 
+//Code to manage the map data..
 const ObjectMapAPI = {
     LoadLookupDataFile(SheetName) {
         try {
@@ -31,21 +32,43 @@ const ObjectMapAPI = {
         const astData = ObjectMapData['AssetTypes'];
         const returnArray = [];
         for (var a in astData) {
-            const rec = astData[a];            
+            const rec = astData[a];
             const st = rec["Study Tags"];
 
             for (let index = 0; index < st.length; index++) {
-                
+
                 const element = st[index];
 
                 //Hey look a match! Add it to our return array....
-                if (element == LinkID) { 
+                if (element == LinkID) {
                     returnArray.push(rec.Name);
                 }
             }
 
         }
-        
+
+        return returnArray;
+
+    },
+    FindEntityType(LinkID) {
+        const astData = ObjectMapData['EntityTypes'];
+        const returnArray = [];
+        for (var a in astData) {
+            const rec = astData[a];
+            const st = rec["Field 5"]; //This name????
+
+            for (let index = 0; index < st.length; index++) {
+
+                const element = st[index];
+
+                //Hey look a match! Add it to our return array....
+                if (element == LinkID) {
+                    returnArray.push(rec.Name);
+                }
+            }
+
+        }
+
         return returnArray;
 
     },
@@ -55,11 +78,169 @@ const ObjectMapAPI = {
     SaveData() {
         DATA_FOLDER.Disk.SaveData('MasterMap', ObjectMapData);
         console.log('The "MasterMap.json" has been written!');
-    }
+    },
+
+    /*
+        Loop through each row and fix stuff...
+    */
+    // ******************************************************
+    FixAssestsData() {
+        const AssetsData = ObjectMapData['Assets'];
+
+        //Just test first row...
+        // const testrow = AssetsData[0];
+        // FixRow(testrow);
+        //return;
+
+        FixRows();
+
+        //Go through all the rows!
+        function FixRows() {
+            for (let index = 0; index < AssetsData.length; index++) {
+                const row = AssetsData[index];
+                FixRow(row);
+            }
+        }
+
+        //fix a single row...
+        function FixRow(RowOfData) {
+            for (var f in RowOfData) {
+
+                const field = RowOfData[f];
+
+                const value2Lookup = field[0];
+
+                //Lookups are of type object....
+                if (typeof (field) == "object") {
+
+                    // const fldName = f.replace(/ /g, '');
+
+                    //Lets do special study tags...
+                    if (f == "Study Tags") {
+
+                        const foundRecord = ObjectMapAPI.FindStudyTag(value2Lookup);
+
+                        if (!foundRecord) {
+                            console.log('Record Key Not Found: ' + value2Lookup);
+
+                        } else {
+                            // Reset object with new lookup!
+                            RowOfData[f] = foundRecord;
+
+                        }
+                    }//End if Study Tag...                
+                }//End  if field is an object....
+            }// End for loop...
+        }
+    },
+
+    FixAssestTypesData() {
+        const AssetTypessData = ObjectMapData['AssetTypes'];
+
+        //Just test first row...
+        const testrow = AssetTypessData[0];
+        FixRow(testrow);
+        return;
+
+        FixRows();
+
+        //Go through all the rows!
+        function FixRows() {
+            for (let index = 0; index < AssetTypessData.length; index++) {
+                const row = AssetTypessData[index];
+                FixRow(row);
+            }
+        }
+
+        //fix a single row...
+        function FixRow(RowOfData) {
+            for (var f in RowOfData) {
+
+                const field = RowOfData[f];
+
+                const value2Lookup = field[0];
+
+                //Lookups are of type object....
+                if (typeof (field) == "object") {
+
+                    // const fldName = f.replace(/ /g, '');
+
+                    //Lets do special study tags...
+                    if (f == "Study Tags") {
+
+                        const foundRecord = ObjectMapAPI.FindStudyTag(value2Lookup);
+
+                        if (!foundRecord) {
+                            console.log('Record Key Not Found: ' + value2Lookup);
+
+                        } else {
+                            // Reset object with new lookup!
+                            RowOfData[f] = foundRecord;
+
+                        }
+                    }//End if Study Tag...                
+                }//End  if field is an object....
+            }// End for loop...
+        }
+    },
+
+    FixEntityTypesData() {
+        const EntityData = ObjectMapData['EntityTypes'];
+
+        //Just test first row...
+        // const testrow = EntityData[0];
+        // FixRow(testrow);
+        // return;
+
+
+        FixRows();
+
+        //Go through all the rows!
+        function FixRows() {
+            for (let index = 0; index < EntityData.length; index++) {
+                const row = EntityData[index];
+                FixRow(row);
+            }
+        }
+
+        //fix a single row...
+        function FixRow(RowOfData) {
+            for (var f in RowOfData) {
+
+                const field = RowOfData[f];
+
+                const value2Lookup = field[0];
+
+                //Lookups are of type object....
+                if (typeof (field) == "object") {
+
+                    // const fldName = f.replace(/ /g, '');
+
+                    //Yikes what is thes???? 
+                    if (f == "Field 5") {
+
+                        const foundRecord = ObjectMapAPI.FindEntityType(value2Lookup);
+
+                        if (!foundRecord) {
+                            console.log('Record Key Not Found: ' + value2Lookup);
+
+                        } else {
+                            // Reset object with new lookup!
+                            RowOfData[f] = foundRecord;
+                        }
+                    }//End if Study Tag...                
+                }//End  if field is an object....
+            }// End for loop...
+        }
+    },
+    // ******************************************************
 };
 
 
 
+console.log(' ****************************************************** ');
+console.log('This is still in development! Most of it is broke!');
+console.log(' ****************************************************** ');
 
 
 //Load up all of our lookup files and keep it in memory...
@@ -68,66 +249,23 @@ ObjectMapAPI.LoadLookupDataFile('AssetTypes');
 ObjectMapAPI.LoadLookupDataFile('EntityTypes');
 ObjectMapAPI.LoadLookupDataFile('StudyTags');
 
-/*
-    Loop through each row and fix it...
-*/
-function FixAssestsData(SheetData) {
-
-    const AssetsData = ObjectMapData['Assets'];
-    
-    //Just test first row...
-    const testrow = AssetsData[0];
-    
-
-    FixRow(testrow);
-    return;
-
-    //Go through all the rows!
-    function FixRows(){
-        for (let index = 0; index < AssetsData.length; index++) {
-            const row = AssetsData[index];
-            FixRow(row);            
-        }        
-    }
-
-    //fix a single row...
-    function FixRow(RowOfData) {
-        for (var f in RowOfData) {
-
-            const field = RowOfData[f];
-            
-            const value2Lookup = field[0];
-
-            //Lookups are of type object....
-            if (typeof (field) == "object") {
-              
-                // const fldName = f.replace(/ /g, '');
-                
-                //Lets do special study tags...
-                if (f == "Study Tags") {
-
-                    const foundRecord = ObjectMapAPI.FindStudyTag(value2Lookup);
-
-                    if (!foundRecord) {
-                        console.log('Record Key Not Found: ' + value2Lookup);
-
-                    } else {
-                        // Reset object with new lookup!
-                        RowOfData['Study Tags'] = foundRecord;
-
-                    }
-                }//End if Study Tag...                
-            }//End  if field is an object....
-        }// End for loop...
-    }
-
-}
-
 
 // We know of at least this one that needs to be fixed...
-FixAssestsData();
- 
+// ObjectMapAPI.FixAssestsData();
+ObjectMapAPI.FixAssestTypesData();
+// ObjectMapAPI.FixEntityTypesData();
 
-ObjectMapAPI.SaveData();
- 
+
+//No longer needed!!!!
+delete ObjectMapData['StudyTags'];
+
+const testData = ObjectMapData['AssetTypes'];
+
+// console.log(ObjectMapData);
+// console.log(testData);
+
+
+
+// ObjectMapAPI.SaveData();
+
 
